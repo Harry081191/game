8ca58dc91bf1a7fbe168dfe51c4a599482dfd896
Harry081191 <harry081191@email.address>
2023-03-21 17:04:52 +0800

    1

--- register.php
+++ register.php
@@ -21,10 +21,10 @@
           </div>
       </nav>
       <div>
-        <form action="test.php" method="get">
+        <form method="get">
             <div class="form-group">
               <label for="username">電子郵件</label>
-              <input type="text" class="form-control" id="username" name="username" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="請輸入電子信箱" required>
+              <input type="text" class="form-control" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" placeholder="請輸入電子信箱" required>
             </div>
             <div class="form-group">
               <label for="username">ID</label>
@@ -38,4 +38,25 @@
         </form>
       </div>
   </body>
+  <?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+    $email = $_GET['email'];
+
+    // 連接到數據庫並檢查用戶記錄
+    // 假設我們已經創建了一個名為 users 的數據庫表
+    $connection = mysqli_connect('localhost', 'root', '', 'test');
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($connection, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+        header('Location: test.php?username=' . urlencode($username) . '&password=' . urlencode($password));
+        exit;
+    } else {
+        header('Location: test.html');
+        exit;
+    }
+}
+?>
 </html>
