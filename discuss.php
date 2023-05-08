cd88d572d64d4857ac55f2d3de091896cb30d470
Harry081191 <harry081191@email.address>
2023-05-08 13:15:01 +0800

    1

--- discuss.php
+++ discuss.php
@@ -8,31 +8,29 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="main.css">
   </head>
   <body>
       <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
-          <a class="navbar-brand" href="#">Navbar</a>
           <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
               aria-expanded="false" aria-label="Toggle navigation"></button>
           <div class="collapse navbar-collapse" id="collapsibleNavId">
               <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                  <li class="nav-item active">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="#">Link</a>
-                  </li>
                   <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                      <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">職業討論版</a>
                       <div class="dropdown-menu" aria-labelledby="dropdownId">
-                          <a class="dropdown-item" href="#">Action 1</a>
-                          <a class="dropdown-item" href="#">Action 2</a>
+                          <a class="dropdown-item" href="#">戰士討論區</a>
+                          <a class="dropdown-item" href="#">射手討論區</a>
+                          <a class="dropdown-item" href="#">法師討論區</a>
+                          <a class="dropdown-item" href="#">巫師討論區</a>
                       </div>
+                  </li>
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#">官方通知區 <span class="sr-only">(current)</span></a>
                   </li>
               </ul>
               <form class="form-inline my-2 my-lg-0">
-                  <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                  <a class="nav-link" href="test.php" style="color: white;">我的首頁 <span class="sr-only">(current)</span></a>
               </form>
           </div>
       </nav>
